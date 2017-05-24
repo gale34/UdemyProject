3be88226b025ccad4b67ecaa371bc6f128f2455d
gale34 <gales34@gmail.com>
2017-05-25 02:45:53 +0900

    170524
    ...

--- C# Unity - Ben Tristem/Number Wizard/Number Wizard/Assets/Scripts/NumberWizards.cs
+++ C# Unity - Ben Tristem/Number Wizard/Number Wizard/Assets/Scripts/NumberWizards.cs
@@ -4,35 +4,64 @@ using UnityEngine;
 
 public class NumberWizards : MonoBehaviour
 {
+    int max;
+    int min;
+    int guess;
 
     // Use this for initialization
     void Start()
     {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        guess = 500;
+
+
+        max = max + 1;
+
+        print ("==========================");
         print("Welcome to Number Wizard");
         print("Pick a number in your head, but don`t tell me!");
 
-        int max = 1000;
-        int min = 1;
+
 
         print("The highest number you can pick is " + max);
         print("The lowest number you can pick it " + min);
 
-        print("Is the number higher or lower than 500?");
+        print("Is the number higher or lower than " + guess + "?");
         print("Up = higher, down = lower, return = equal");
-
-
     }
-
+   
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.UpArrow))
         {
-            print("up arrow pressed");
+            //print("up arrow pressed");
+            min = guess;
+            NextGuess();
         }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             print("down arrow pressed");
+            max = guess;
+            NextGuess();
         }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            print("I won!");
+            StartGame();
+        }
+    }
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        print("Higher or lower than" + guess);
+        print("Up = higher, down = lower, return = equal");
     }
 }

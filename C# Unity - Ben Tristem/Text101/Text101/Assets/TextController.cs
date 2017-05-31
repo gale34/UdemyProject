aa45a7b69b2bd3754ce16c93ce549b6764f7fb9a
gale34 <gales34@gmail.com>
2017-06-01 01:31:30 +0900

    170531
    조금만...

--- C# Unity - Ben Tristem/Text101/Text101/Assets/TextController.cs
+++ C# Unity - Ben Tristem/Text101/Text101/Assets/TextController.cs
@@ -16,7 +16,10 @@ public class TextController : MonoBehaviour {
 	void Update () {
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            text.text = "Space key pressed";
+            text.text = "You are in a prison cell, and you want to escape. There are " +
+                "some dirty sheets on the bed, a mirror on the wall, and the door " +
+                "is locked from the outside.\n\n" +
+                "Press S to View Sheets, M to view Mirror and L to view Lock";
         }
             
     }

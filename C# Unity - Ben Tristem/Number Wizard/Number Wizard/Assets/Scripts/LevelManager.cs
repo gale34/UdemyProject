a66a86a63ad3d191b100de97938257b68adea480
gale34 <gales34@gmail.com>
2017-06-06 19:12:27 +0900

    170606
    ...

---
+++ C# Unity - Ben Tristem/Number Wizard/Number Wizard/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name)
+    {
+        Debug.Log("Level load requested for: " + name);
+        //Application.LoadLevel(name);
+        SceneManager.LoadScene(name);
+       // SceneManager.
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("I Want to quit!");
+        Application.Quit();
+    }
+}

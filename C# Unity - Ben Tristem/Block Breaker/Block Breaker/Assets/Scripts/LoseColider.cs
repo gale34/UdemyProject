1683e0318cf9347bcd3ee6620c94fa0b73c9e2c9
gale34 <gales34@gmail.com>
2017-06-29 18:16:08 +0900

    170629
    ...

---
+++ C# Unity - Ben Tristem/Block Breaker/Block Breaker/Assets/Scripts/LoseColider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseColider : MonoBehaviour {
+
+    public LevelManager levelManager;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("Trigger");
+        SceneManager.LoadScene("Win");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("Collision");
+    }
+
+}
